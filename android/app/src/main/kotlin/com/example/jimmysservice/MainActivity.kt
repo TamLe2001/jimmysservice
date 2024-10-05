5744566a49febd5d1b7ca2cf82c99be34401d7fb
TamLe2001 <TamLe2001@live.dk>
2024-10-05 22:02:48 +0200

    replacement

---
+++ android/app/src/main/kotlin/com/example/jimmysservice/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jimmysservice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- mindou/android/app/src/main/kotlin/com/example/mindou/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mindou
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
