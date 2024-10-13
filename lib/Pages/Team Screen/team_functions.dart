09b13ffa32234cc44454c5dab39d9c5403e982f0
TamLe2001 <TamLe2001@live.dk>
2024-10-13 11:21:16 +0200

    add player more fancy

--- lib/Pages/Team Screen/team_functions.dart
+++ lib/Pages/Team Screen/team_functions.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:jimmysservice/Classes/Player/player.dart';
 import 'package:jimmysservice/Classes/Team/team.dart';
@@ -35,13 +37,15 @@ class TeamFunctions {
               ),
               TextButton(
                 onPressed: () {
-                  players.add(
-                    Minion(
-                      name: newPlayer.text,
-                    ),
-                  );
-                  playerLength.value = players.length;
-                  newPlayer.text = '';
+                  if (newPlayer.text.isNotEmpty) {
+                    players.add(
+                      Minion(
+                        name: newPlayer.text,
+                      ),
+                    );
+                    playerLength.value = players.length; // Update ValueNotifier
+                    newPlayer.text = ''; // Reset text field
+                  }
                   Navigator.of(context).pop();
                 },
                 child: const Text('OK'),
@@ -51,5 +55,73 @@ class TeamFunctions {
         },
       );
     };
+  }
+
+  List<Draggable<String>> listBuilder() {
+    Color getRandomColor() {
+      Random random = Random();
+      return Color.fromARGB(
+        255,
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+      );
+    }
+
+    CircleAvatar playerIcon(String label, Color color) {
+      return CircleAvatar(
+        backgroundColor: color,
+        radius: 30,
+        child: Text(
+          label.toUpperCase(),
+          style: const TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+          ),
+        ),
+      );
+    }
+
+    Map<String, int> firstLetterCount = {};
+    for (var player in players) {
+      String firstLetter =
+          player.name.isNotEmpty ? player.name.substring(0, 1) : "?";
+      firstLetterCount[firstLetter] = (firstLetterCount[firstLetter] ?? 0) + 1;
+    }
+
+    List<Draggable<String>> playerIcons = players.map((player) {
+      String name = player.name;
+      String label = "";
+      Color randomColor = getRandomColor();
+
+      if (name.isNotEmpty) {
+        String firstLetter = name.substring(0, 1);
+
+        if (firstLetterCount[firstLetter]! == 2) {
+          label = name.length > 1 ? name.substring(0, 2) : firstLetter;
+        } else if (firstLetterCount[firstLetter]! > 2) {
+          label = name.length > 2 ? name.substring(0, 3) : name;
+        } else {
+          label = firstLetter;
+        }
+      } else {
+        label = "?";
+      }
+
+      final widget = playerIcon(label, randomColor);
+
+      return Draggable<String>(
+        data: player.name,
+        feedback: widget,
+        childWhenDragging: Opacity(
+          opacity: 0.5,
+          child: widget,
+        ),
+        onDragCompleted: () {},
+        child: widget,
+      );
+    }).toList();
+
+    return playerIcons;
   }
 }
--- lib/Pages/Team Screen/team_screen.dart
+++ lib/Pages/Team Screen/team_screen.dart
@@ -9,20 +9,20 @@ class TeamScreen extends StatefulWidget {
 }
 
 class TeamScreenState extends State<TeamScreen> {
+  late TeamFunctions teamFunctions;
+
+  @override
+  void initState() {
+    super.initState();
+    teamFunctions = TeamFunctions(context: context);
+  }
+
   double screenWidth() {
     return MediaQuery.of(context).size.width;
   }
 
   double screenHeight() {
     return MediaQuery.of(context).size.height;
-  }
-
-  Future<TeamFunctions?> teamFunctionsCreation() async {
-    if (mounted) {
-      return TeamFunctions(context: context);
-    } else {
-      return null;
-    }
   }
 
   @override
@@ -39,112 +39,81 @@ class TeamScreenState extends State<TeamScreen> {
           ),
         ),
       ),
-      body: FutureBuilder<TeamFunctions?>(
-        future: teamFunctionsCreation(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text("Error: ${snapshot.error}"),
-            );
-          } else if (snapshot.hasData && snapshot.data != null) {
-            TeamFunctions teamFunctions =
-                snapshot.data ?? TeamFunctions(context: context);
-            return ValueListenableBuilder<int>(
-              valueListenable: teamFunctions.playerLength,
-              builder: (context, value, child) {
-                return Center(
-                  child: Column(
+      body: ValueListenableBuilder<int>(
+        valueListenable: teamFunctions.playerLength,
+        builder: (context, value, child) {
+          return Center(
+            child: Column(
+              children: [
+                Flexible(
+                  child: Row(
                     children: [
-                      Flexible(
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                color: Colors.red,
-                                height: double.infinity,
-                                width: double.infinity,
-                              ),
-                            ),
-                            Container(
-                              color: Colors.green,
-                              width: screenWidth() * 0.2,
-                            ),
-                            Expanded(
-                              child: Container(
-                                color: Colors.blue,
-                                height: double.infinity,
-                                width: double.infinity,
-                              ),
-                            ),
-                          ],
+                      Expanded(
+                        child: Container(
+                          color: Colors.red,
+                          height: double.infinity,
+                          width: double.infinity,
                         ),
                       ),
                       Container(
-                        color: Colors.grey,
-                        width: double.infinity,
-                        child: GestureDetector(
-                          onTap: teamFunctions.addPlayer(),
-                          child: Center(
-                            child: Card(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: screenWidth() * 0.02,
-                                  vertical: screenHeight() * 0.001,
-                                ),
-                                child: Text(
-                                  "Players: $value",
-                                  style: TextStyle(
-                                    fontSize: screenWidth() * 0.016,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
+                        color: Colors.green,
+                        width: screenWidth() * 0.2,
                       ),
-                      Container(
-                        height: screenHeight() * 0.23,
-                        color: Colors.white,
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Center(
-                            child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: value,
-                              itemBuilder: (context, index) {
-                                return Padding(
-                                  padding: EdgeInsets.all(5),
-                                  child: CircleAvatar(
-                                    backgroundColor: Colors.grey.shade200,
-                                    child: Text(
-                                      "A",
-                                      style: TextStyle(
-                                        fontSize: 24,
-                                        color: Colors.black,
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
+                      Expanded(
+                        child: Container(
+                          color: Colors.blue,
+                          height: double.infinity,
+                          width: double.infinity,
                         ),
                       ),
                     ],
                   ),
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: Text("No data available"),
-            );
-          }
+                ),
+                Container(
+                  color: Colors.grey,
+                  width: double.infinity,
+                  child: GestureDetector(
+                    onTap: teamFunctions.addPlayer(),
+                    child: Center(
+                      child: Card(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: screenWidth() * 0.02,
+                            vertical: screenHeight() * 0.001,
+                          ),
+                          child: Text(
+                            "Players: $value",
+                            style: TextStyle(
+                              fontSize: screenWidth() * 0.016,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: screenHeight() * 0.23,
+                  color: Colors.white,
+                  child: Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: teamFunctions.listBuilder(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
         },
       ),
     );
